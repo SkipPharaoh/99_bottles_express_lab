8f5972c714af1ca1b2f0cadaab1375d124f6bb77
wsjoshua <ws.joshua@gmail.com>
2021-05-04 15:50:04 -0600

    updated solution

---
+++ server.js
@@ -0,0 +1,32 @@
+// APP IMPORTS
+const express = require('express')
+const app = express()
+// GLOBAL VALUES
+
+let lastEntry = 99
+// MIDDLEWARE / CONFIGURATION
+
+app.set('view engine', 'ejs');
+
+// ROUTING
+
+app.get('/', (req,res)=>res.redirect('/bottles/99'))
+app.get('/bottles/:id',(req, res)=>{
+    // uses a ternary to convert the string into a number and checks if it is a greater than one, if not will set it back to 99
+    const bottles = parseInt(req.params.id) >= 1 ? parseInt(req.params.id) : 99
+    // sets the next variable to either the current bottle count or 99 
+    const next = bottles-1 > 0 ? bottles-1 : 99;
+    // sets the url number for the next round
+    const validNum = !isNaN(req.params.id)
+    // uses isNaN to check if id is a number -- filters for strings entered manually 
+    if(validNum){
+        res.render('index', {bottles, next})
+    }else {
+        // If not valid string, redirect to most recent valid string (default: 99)
+        res.redirect(`/bottles/${lastEntry}`)
+    }  
+    lastEntry = bottles
+})
+
+// INITIALIZE SERVER
+app.listen(9000,()=>console.log('listening on port 9000'))
